d3d2a16b9919fb35c238c244d020b832c87ef874
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-12-14 11:55:44 +0200

    connect the login route, get it working

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import router from "./routes/authRoutes";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO_URI || '')
     })
 
 app.use(express.json());
+app.use(cors());
 app.use("/auth", router);
 
 app.get('/', (req, res) => {
